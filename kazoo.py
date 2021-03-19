c1c0e38dd8ceb343fe4283a37e476fd24fe81340
Daniel Inhee Lee <lee.danny112358@gmail.com>
2021-03-18 22:05:31 -0700

    finalized

--- kazoo.py
+++ kazoo.py
@@ -12,20 +12,30 @@ training_dataset = {
   "spectrogram": [],
   "note": []
 }
+testing_dataset = {
+  "spectrogram": [],
+  "note": []
+}
 
 # loop through all data and preprocess
-def process_data(path):
+def process_data(path,dataset):
   for i, (dirpath, dirnames, filenames) in enumerate(os.walk(path)):
     if dirpath != path:
       print("Processing {} notes".format(dirpath))
       for file in filenames:
         if file != ".DS_Store":
-          training_dataset["note"].append(file)
           file_path = os.path.join(dirpath,file)
           signal,sr = librosa.load(file_path,sr=22050)
           stft = librosa.core.stft(signal[0:45050], n_fft=2047, hop_length=512)
           spectrogram = np.abs(stft)
-          training_dataset["spectrogram"].append(spectrogram)   
+          row, col = spectrogram.shape
+          if col == 88:
+            if dataset == "training":
+              training_dataset["spectrogram"].append(spectrogram)   
+              training_dataset["note"].append(file)
+            if dataset == "testing":
+              testing_dataset["spectrogram"].append(spectrogram)
+              testing_dataset["note"].append(file)
 
 def pre_process(X):
   norm_data = []
@@ -42,25 +52,40 @@ def add_noise(specs):
   return specs
 
 # NOTE: Please refer to the following blocks of code to test out the different experimentations
-experiment = 1
+print("0 - vanilla note generation\n")
+print("1 - train noise / test clean\n")
+print("2 - train clean / test noise\n")
+print("3 - train notes / test chords\n")
+print("4 - train chords / test notes\n")
+experiment = int(input("Enter experiment number: "))
+
 # vanilla notes
 if experiment == 0:
-  process_data("./notes")
+  process_data("./dataset/experiment012","training")
   X_train  =  pre_process(np.array(training_dataset["spectrogram"][100:1000]))
   X_test  =  pre_process(np.array(training_dataset["spectrogram"][0:100]))
 # train noise / test clean
 if experiment ==  1:
-  process_data("./notes")
+  process_data("./dataset/experiment012","training")
   X_train = pre_process(np.array(add_noise(training_dataset["spectrogram"][100:1000])))
   X_test = pre_process(np.array(training_dataset["spectrogram"][0:100]))
 # trained clean / test noise
 if experiment == 2:
-  process_data("./notes")
+  process_data("./dataset/experiment012","training")
   X_train = pre_process(np.array(training_dataset["spectrogram"][100:1000]))
   X_test = pre_process(np.array(add_noise(training_dataset["spectrogram"][0:100])))
-
-print(X_train.shape)
-print(X_test.shape)
+# trained notes / test chords
+if experiment == 3:
+  process_data("./dataset/experiment3_notes","training")
+  X_train = pre_process(np.array(training_dataset["spectrogram"]))
+  process_data("./dataset/experiment3_chords","testing")
+  X_test  = pre_process(np.array(testing_dataset["spectrogram"]))
+# trained chords /test notes
+if experiment == 4:
+  process_data("./dataset/experiment4_chords","training")
+  X_train = pre_process(np.array(training_dataset["spectrogram"]))
+  process_data("./dataset/experiment4_notes","testing")
+  X_test  = pre_process(np.array(testing_dataset["spectrogram"]))
 
 def show_data(X, n=10, height=88, width=1024, title=""):
     plt.figure(figsize=(5, 5))
@@ -102,21 +127,32 @@ AE.fit(X_train, X_train,
 AE.save("kazoo.h5")
 decoded_data = AE.predict(X_test)
 
-for i in range(25):
+for i in range(10): #get 10 input/output pairs to show as examples for project
   synth_sound = decoded_data[i].reshape(1024,88)
   original_sound = X_test[i].reshape(1024,88)
   log_synth_sound = librosa.amplitude_to_db(synth_sound)
   log_original_sound = librosa.amplitude_to_db(original_sound)
   librosa.display.specshow(log_synth_sound,sr=22050,hop_length=512)
-  title = "Generated Spec for {}".format(training_dataset["note"][i])
+  if experiment <= 2:
+    title = "Generated Spec for {}".format(training_dataset["note"][i])
+  else:
+    title = "Generated Spec for {}".format(testing_dataset["note"][i])
   plt.suptitle(title, fontsize = 20)
   plt.xlabel("time")
   plt.ylabel("frequency")
   plt.show()
   librosa.display.specshow(log_original_sound,sr=22050,hop_length=512)
-  title = "Original Spec for {}".format(training_dataset["note"][i])
+  if experiment <= 2:
+    title = "Original Spec for {}".format(training_dataset["note"][i])
+  else:
+    title = "Original Spec for {}".format(testing_dataset["note"][i])
   plt.suptitle(title, fontsize = 20)
+  plt.xlabel("time")
+  plt.ylabel("frequency")
   plt.show()
   audio = librosa.istft(synth_sound, hop_length=512)
-  name = "synth_{}.wav".format(training_dataset["note"][i])
+  if experiment <=2:
+    name = "synth_{}".format(training_dataset["note"][i])
+  else:
+    name = "synth_{}".format(testing_dataset["note"][i])
   sf.write(name,audio,22050)
